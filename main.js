65f339760d4d76b6ca813622916a6599be353f0b
RyotaroIeda <109324447+RyotaroIeda@users.noreply.github.com>
2022-07-29 17:57:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -11,12 +11,70 @@ function buttonPress(event) {
     calculator = eval(calculator);
   } else if (last === "AC") {//ACをクリックした場合
     calculator = '0';
-  } else if (last == operator) {//演算子をクリックした場合
-    if (calculator.innerHTML.slice(-1) == operator) {
-      calculator.innerHTML = calculator.innerHTML.slice(0,-1);
-    } else {
-      calculator += last;
-    }
+  } else if (calculator.slice(-1) == '+') {//+演算子
+      if (last == '+') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '-') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '*') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '/') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else {
+        calculator += last;
+      } 
+  } else if (calculator.slice(-1) == '-') {//-演算子
+      if (last == '+') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '-') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '*') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '/') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else {
+        calculator += last;
+      }
+  } else if (calculator.slice(-1) == '*') {//*演算子
+      if (last == '+') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '-') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '*') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '/') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else {
+        calculator += last;
+      }
+  } else if (calculator.slice(-1) == '/') {///演算子
+      if (last == '+') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '-') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '*') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else if (last == '/') {
+        calculator = calculator.slice(0,-1);
+        calculator += last;
+      } else {
+        calculator += last;
+      }
   }
   else {//数字をクリックした場合
     if (calculator == '0' || calculator == '00') {
@@ -27,3 +85,6 @@ function buttonPress(event) {
   }
   result.textContent = calculator;
 }
+
+
+
