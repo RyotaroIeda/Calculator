1f22ca06c12d39c69d09037228fa7fad6ec23ecb
RyotaroIeda <109324447+RyotaroIeda@users.noreply.github.com>
2022-07-29 15:27:18 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,28 @@ const buttons = document.querySelectorAll('button');
 let result = document.getElementById('result');
 buttons.forEach(button => button.addEventListener('click', buttonPress))
 //ボタンクリック時
-let calculator = '';
+let calculator = '0';
+
 function buttonPress(event) {
-  const last = event.target.textContent;
+  const last = event.target.innerHTML;
   const operator = document.getElementsByClassName('operator');
   if (last === "=") {//イコールをクリックした場合   
     calculator = eval(calculator);
   } else if (last === "AC") {//ACをクリックした場合
-    calculator = '';
-  } else if (last === operator && calculator.innerHTML.slice(-1) === operator) {
-    calculator = calculator.innerHTML.slice(0,-1);
-    calculator += last;
+    calculator = '0';
+  } else if (last == operator) {//演算子をクリックした場合
+    if (calculator.innerHTML.slice(-1) == operator) {
+      calculator.innerHTML = calculator.innerHTML.slice(0,-1);
+    } else {
+      calculator += last;
+    }
   }
   else {//数字をクリックした場合
-    calculator += last;
-  } 
-  result.textContent = calculator;//ディスプレイに計算結果表示
-  console.log(calculator);
+    if (calculator == '0' || calculator == '00') {
+      calculator = last;
+    } else{
+      calculator += last;
+    }
+  }
+  result.textContent = calculator;
 }
